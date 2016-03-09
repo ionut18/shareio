b4c8c2dca7ae7eec59c383cbddc358ea4044f441
ionut <ionut.pic@gmail.com>
2016-03-09 22:55:33 +0200

    Initial commit

---
+++ src/main/java/org/licence/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.licence.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ionut on 09.03.2016.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello/")
+    public String getHello(){
+        return "hello";
+    }
+
+}
